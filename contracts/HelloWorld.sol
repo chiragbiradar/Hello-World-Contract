075c5c25d12b4d5cd2f22923330614eb7b7709fb
chiragbiradar <chiragsb16@gmail.com>
2022-07-16 10:02:15 +0530

    add all files

---
+++ contracts/HelloWorld.sol
@@ -0,0 +1,25 @@
+//SPDX-License-Identifier: UNLICENSED
+
+pragma solidity >= 0.8.9;
+
+contract HelloWorld {
+    //events
+    //states
+    //functions
+
+    event messagechanged(string oldmsg, string newmsg);
+
+    string public message;
+
+    constructor(string memory firstmessage) {
+        message = firstmessage;   
+    }
+
+    function update(string memory newmesssage) public {
+        string memory oldmsg = message;
+        message = newmesssage;
+
+        emit messagechanged(oldmsg, newmesssage);
+
+    }
+}
